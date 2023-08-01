5643a77fc62cb77b3f613f22d08c3aab17d03367
Jalba Bianca <jalbabianca@Jalbas-MacBook-Air.local>
2023-08-01 17:14:23 +0300

    first commit

---
+++ src/Cart.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import Input from "./Input";
+import Button from "./Button";
+
+// TODO: Replace with your own publishable key
+const stripeLoadedPromise = loadStripe("pk_test_51NYUGiE7bYzcHE0JLABW2ZSySuFyOQ1SHWcburbFIRixZzQYCCjc8SI6LP8qpVQ5ccPCu8GlCiKYOagaEIv5PONv0033gR8dDp");
+
+export default function Cart({ cart }) {
+  const totalPrice = cart.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  const [email, setEmail] = useState("");
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const lineItems = cart.map((product) => {
+      console.log(product, "map");
+      return { price: product.price_id, quantity: product.quantity };
+    });
+    console.log(lineItems);
+
+    stripeLoadedPromise.then((stripe) => {
+      console.log(stripe);
+      stripe
+        .redirectToCheckout({
+          lineItems: lineItems,
+          mode: "payment",
+          successUrl: "http://localhost:5173/",
+          cancelUrl: "http://localhost:5173/",
+          customerEmail: email,
+        })
+        .then((response) => {
+          // this will only log if the redirect did not work
+          console.log(response.error);
+        })
+        .catch((error) => {
+          // wrong API key? you will see the error message here
+          console.log(error);
+        });
+    });
+  }
+
+  return (
+    <div className="cart-layout">
+      <div>
+        <h1>Your Cart</h1>
+        {cart.length === 0 && (
+          <p>You have not added any product to your cart yet.</p>
+        )}
+        {cart.length > 0 && (
+          <>
+            <table className="table table-cart">
+              <thead>
+                <tr>
+                  <th width="25%" className="th-product">
+                    Product
+                  </th>
+                  <th width="20%">Unit price</th>
+                  <th width="10%">Quanity</th>
+                  <th width="25%">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((product) => {
+                  return (
+                    <tr key={product.id}>
+                      <td>
+                        <img
+                          src={product.image}
+                          width="30"
+                          height="30"
+                          alt=""
+                        />{" "}
+                        {product.name}
+                      </td>
+                      <td>${product.price}</td>
+                      <td>{product.quantity}</td>
+                      <td>
+                        <strong>${product.price * product.quantity}</strong>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th colSpan="2"></th>
+                  <th className="cart-highlight">Total</th>
+                  <th className="cart-highlight">${totalPrice}</th>
+                </tr>
+              </tfoot>
+            </table>
+            <form className="pay-form" onSubmit={handleFormSubmit}>
+              <p>
+                Enter your email and then click on pay and your products will be
+                delivered to you on the same day!
+                <br />
+                <em>
+                  Enter your own Stripe Publishable Key in Cart.js for the
+                  checkout to work
+                </em>
+              </p>
+              <Input
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                type="email"
+                required
+              />
+              <Button type="submit">Pay</Button>
+            </form>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/Products.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import Product from "./Product";
+import useFetch from "./useFetch";
+import Loader from "./Loader";
+
+export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const { get, loading } = useFetch(
+    "https://supermarket-ce173-default-rtdb.europe-west1.firebasedatabase.app/"
+  );
+
+  useEffect(() => {
+    get("supermarket.json")
+      .then((data) => {
+        setProducts(data);
+        console.log(data);
+      })
+      .catch((error) => console.log("Could not load products", error));
+  }, []);
+
+  return (
+    <div className="products-layout">
+      <h1>Products</h1>
+      <p>Take a look at our products</p>
+      <div className="products-grid">
+        {loading && <Loader />}
+        {products.map((product) => {
+          console.log(product);
+          return (
+            <Product
+              key={product.id}
+              details={product}
+              cart={props.cart}
+              onProductAdd={props.onProductAdd}
+              onProductDelete={props.onProductDelete}
+            ></Product>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
