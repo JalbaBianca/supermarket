f45dac13763973e364292f72a2497768efb2dfa0
Jalba Bianca <jalbabianca@Jalbas-MacBook-Air.local>
2023-08-10 11:47:06 +0300

    ChangeTheme

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Navbar(props) {
+  const [isDarkTheme,  setIsDarkTheme] = useState (false);
+
   const cartCount = props.cart.reduce(
     (total, product) => total + product.quantity,
     0
   );
+
+  useEffect(()=>{
+    if(isDarkTheme){
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  })
+
+  function handleThemeClick() {
+    setIsDarkTheme(!isDarkTheme);
+  }
 
   return (
     <nav className="navbar">
@@ -12,6 +27,11 @@ export default function Navbar(props) {
         SuperMarket
       </NavLink>
       <ul>
+        <li className="nav-item">
+          <Button className="theme-swhitcher" onClick={handleThemeClick}>
+            {isDarkTheme ? "Dark" : "Light"}
+          </Button>
+        </li>
         <li className="nav-item">
           <NavLink
             className={({ isActive }) => (isActive ? "active" : "")}
